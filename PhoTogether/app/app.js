1d316bc36519f1487111f3cd37f53717f282adc5
Nikola Irinchev <nikola.irinchev@telerik.com>
2016-06-13 14:49:07 +0000

    Filter out non-photogether events

--- PhoTogether/app/app.js
+++ PhoTogether/app/app.js
@@ -5,12 +5,12 @@ application.cssFile = "./app.css";
 
 // START_CUSTOM_CODE_nativeScriptApp
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-let Analytics = require('nativescript-telerik-analytics');
+//let Analytics = require('nativescript-telerik-analytics');
 
 application.on(application.launchEvent, function(context) {
     // Initialize the Analytics plugin and start a new session when the application starts
-    Analytics.init({ appId: 'xhpeizqd4hnqef1n' });
-    Analytics.start();
+    //Analytics.init({ appId: 'xhpeizqd4hnqef1n' });
+    //Analytics.start();
 });
 
 // END_CUSTOM_CODE_nativeScriptApp
