398c3c7c39bc6ea818a512e17ac56e160316c9c9
Todor Totev <todor.totev@telerik.com>
2016-06-13 12:09:10 +0000

    Add Telerik Analytics

--- PhoTogether/app/components/homeView/homeView.js
+++ PhoTogether/app/components/homeView/homeView.js
@@ -1,9 +1,13 @@
+"use strict";
+
 var modelModule = require("./homeView-service");
 var model = modelModule.photoAlbumModel;
 
 function onPageLoaded(args) {
   var page = args.object;
   page.bindingContext = model;
+    
+    throw new Exception("Test exception");
 }
 
 exports.onPageLoaded = onPageLoaded;
